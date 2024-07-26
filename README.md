d03600f5eca30134de4a55bde01c5a7c1b574bac
Dennis <dennis.lentschig955@gmail.com>
2024-07-26 17:29:04 +0200

    created basic layout

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Cookbook
 Cookbook webpage using Python and Flask
+
+
+## usage
+```. env/bin/activate```
+```./run.sh```
---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Set the FLASK_APP environment variable
+export FLASK_APP=main
+
+# Run the Flask app
+flask run
